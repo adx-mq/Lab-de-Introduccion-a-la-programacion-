83e1d0656a696930090ab620c640876a513d7f9b
adx-mq <danielaquinm8@gmail.com>
2026-02-22 17:27:45 -0600

    Update Codigo.py

--- Ejercicio_3/Codigo.py
+++ Ejercicio_3/Codigo.py
@@ -1,1 +1,24 @@
+intentos=0
+while (intentos<3):
+    usuario = input("usuario:")
+    password = input("Contraseña:")
+    if usuario =="":
+        print("Usuario vacío")
+        intentos += 1
+    elif usuario==chr(32):
+       print("espacios no permitidos")
+       intentos += 1
+    elif len(password)<8:
+        print("La contraseña es minima a 8 caracteres")
+        intentos += 1
+        for c in password:
+            if not (c.isdigit() or ('a' <= c <= 'z') or ('A' <= c <= 'Z')):
+                print("La contraseña debe contener solo letras y números")
+                break
+    elif usuario == "admin" and password == "Admin2026":
+     print("Acceso concedido")
+     break 
 
+    elif usuario != "admin" or password != "Admin2026":
+        intentos += 1 
+    print("Acceso denegado. Intentos restantes:", 3-intentos)
