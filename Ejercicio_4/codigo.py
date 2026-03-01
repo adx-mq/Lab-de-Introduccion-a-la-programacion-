f3fa6db7d9c70a218827a2c1b97dc7e20689084d
adx-mq <danielaquinm8@gmail.com>
2026-03-01 17:31:37 -0600

    Add menu options after admin access

--- Ejercicio_4/codigo.py
+++ Ejercicio_4/codigo.py
@@ -23,4 +23,27 @@ while (intentos<3):
     elif usuario == "admin" and password == "Admin2026":
         print("Acceso permitido") 
         acceso = 1
-        
+      
+    while acceso == 1:
+        print("Bienvenido al sistema de acceso. Por favor, selecciona una opcion:")
+        print("1. Clasificar numero") 
+        print("2. Categoria edad y permisos")
+        print("3. Calcular tarifa final")
+        print("4. Cerrar sesion")
+        print("5. Salir")
+        opcion = input("Selecciona una opción: ") 
+        if opcion == "1": 
+            print("Clasificar número ")
+        elif opcion  == "2":
+            print("Categoria de edad y permisos")
+        elif opcion == "3":
+            print("Calcular tarifa final")
+        elif opcion == "4":
+            acceso=0
+            break
+        elif opcion == "5":
+            print ("salir")
+            acceso=2
+            if acceso==2:
+                break 
+
