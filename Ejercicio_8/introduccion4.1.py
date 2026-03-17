cb9611c99ca62af1c3be43b9790ea1ac8064bc56
Daniela <danielaquinm8@gmail.com>
2026-03-17 08:53:59 -0600

    Ejercicio_8

---
+++ Ejercicio_8/introduccion4.1.py
@@ -0,0 +1,6 @@
+i= 1
+while True:
+        print ("hola mundo cruel   ",i)
+        i += 1
+        if i > 10:
+            break
---
+++ Ejercicio_8/introduccion4.py
@@ -0,0 +1,9 @@
+interrumpir = int(input("Hasta cual interrumpes ? "))
+i= 1
+while True:
+    if i != 5 :
+        print ("hola mundo cruel   ",i)
+        i = i + 1
+    
+    if i > interrumpir:
+        break
