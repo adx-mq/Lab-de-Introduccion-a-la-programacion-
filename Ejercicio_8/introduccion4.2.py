68b58f5a60b659e12d1a9478493ac2cad44eba60
adx-mq <danielaquinm8@gmail.com>
2026-05-07 07:48:52 -0600

    Delete Ejercicio_8/introduccion4.2.py

--- Ejercicio_8/introduccion4.2.py
+++
@@ -1,8 +0,0 @@
-print("Cual tabla inicial quieres?")
-tablaInicial = int(input())
-print("Cual tabla final quieres?")
-tablaFinal = int(input())
-for i in range(tablaInicial, tablaFinal + 1, 1):
-    print("tabla = " + str(i))
-    for j in range(1, 10 + 1, 1):
-        print(str(i) + " X " + str(j) + "  =  " + str(i + j))
