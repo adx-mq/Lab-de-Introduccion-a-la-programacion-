2521f99f330ae85870ac1319f69f8649814dd7d4
adx-mq <danielaquinm8@gmail.com>
2026-05-07 07:48:29 -0600

    Delete Ejercicio_8/introduccion4.py

--- Ejercicio_8/introduccion4.py
+++
@@ -1,9 +0,0 @@
-interrumpir = int(input("Hasta cual interrumpes ? "))
-i= 1
-while True:
-    if i != 5 :
-        print ("hola mundo cruel   ",i)
-        i = i + 1
-    
-    if i > interrumpir:
-        break
